3d6b20275012b28c8215e380c78ff85d57ca85ff
junhaa <2171326@hansung.ac.kr>
2024-11-15 04:01:04 +0900

    :art: CHORE. 인덱스 통합 및 검색 시 필터 적용
    - 기존 회사 규모 인덱스 필드 통합
    - 검색 시 회사 규모 필터링 추가

--- storage/elastic_search_storage.py
+++ storage/elastic_search_storage.py
@@ -3,7 +3,7 @@ import os
 from embeddings.sentence_transform import sentence_embedding
 from elasticsearch import Elasticsearch
 from enum import Enum
-from constants import ES_PASSWORD, ES_USERNAME, ES_BASE_URL
+from constants import ES_PASSWORD, ES_USERNAME, ES_BASE_URL, ES_INDEX_NAME
 
 # cert 파일 경로
 ES_CA_CERT_PATH = os.path.join(os.path.dirname(__file__), "..", "cert", "http_ca.crt")
@@ -27,15 +27,13 @@ def create_database():
     # json 데이터 정보 매핑
     schema = get_database_schema()
 
-    # 기업 규모 마다 index 생성
-    for size in CompanySize:
-        index_name = get_index_name(size)
-        es.indices.create(index=index_name, body=schema, ignore = 400) # 이미 존재하는 경우 무시
+    es.indices.create(index=ES_INDEX_NAME, body=schema, ignore = 400) # 이미 존재하는 경우 무시
 
 def get_database_schema():
     mapping = {
         "mappings": {
             "properties": {
+                "company_size": {"type": "text"},
                 "question": {"type": "text"},
                 "answer": {"type": "text"},
                 "embedding": {
@@ -51,24 +49,22 @@ def get_database_schema():
 
 def save_qna_data(company_size, question, answer):
 
-    # 기업 규모 기준으로 인덱스 이름 불러오기
-    index_name = get_index_name(company_size)
-
     # 질문 내용 기준으로 임베딩
     embeddings = sentence_embedding(question)
 
-    # 벡터 정규화=
+    # 벡터 정규화
     question_embedding = normalize_embeddings(embeddings)
 
     # Elastic_search 데이터 스키마 형식으로 변경
-    doc = create_doc_data(question, answer, question_embedding)
+    doc = create_doc_data(company_size, question, answer, question_embedding)
 
-    response = es.index(index=index_name, document=doc)
+    response = es.index(index=ES_INDEX_NAME, document=doc)
 
     print("저장된 데이터 : ", response)
 
-def create_doc_data(question, answer, question_embedding):
+def create_doc_data(company_size, question, answer, question_embedding):
     doc = {
+        "company_size": company_size.value,
         "question": question,
         "answer": answer,
         "embedding": question_embedding
@@ -80,19 +76,37 @@ def normalize_embeddings(embeddings):
     norms = np.linalg.norm(embeddings, axis=0, keepdims=True)
     return embeddings / norms
 
-def get_index_name(company_size):
-    if(company_size == CompanySize.MICRO):
-        return "qna_embedding_micro"
-    elif(company_size == CompanySize.SMALL):
-        return "qna_embedding_small"
-    elif(company_size == CompanySize.MEDIUM):
-        return "qna_embedding_medium"
-    else:
-        return "qna_embedding_large"
 
-def get_saved_doc_data(index_name, doc_id):
+def get_saved_doc_data_by_id(index_name, doc_id):
     try:
         response = es.get(index=index_name, id= doc_id)
         print("문서 데이터:", response['_source'])  # 문서의 내용 출력
     except Exception as e:
         print("오류 발생:", e)
+
+def get_similar_qna_data(company_size, ):
+
+
+def create_query(query_vector, company_size, data_size):
+    # 쿼리 작성
+    query = {
+        "size": data_size,  # 검색할 인접 벡터 수
+        "query": {
+            "bool": {
+                "filter": [
+                    {"term": {"company_size": company_size.value}},  # 필터 조건 기업 규모에 맞게
+                ],
+                "must": {
+                    "script_score": {
+                        "query": {"match_all": {}},
+                        "script": {
+                            "source": "cosineSimilarity(params.query_vector, 'vector_field') + 1.0", # 코사인 유사도로 검색
+                            "params": {"query_vector": query_vector}
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return query
