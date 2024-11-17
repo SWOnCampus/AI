caf8e67d7c2346675ce92bd4ba7af618d065109e
junhaa <2171326@hansung.ac.kr>
2024-11-18 00:00:28 +0900

    :sparkles: FEAT.AI 도입 과정 프로세스 내용 생성 기능 구현

--- llm/generate_result.py
+++ llm/generate_result.py
@@ -1,4 +1,4 @@
-from storage.elastic_search_storage import Industry, Category, get_similar_qna_data
+from storage.elastic_search_storage import Industry, Category, get_similar_qna_data, CompanySize
 import openai
 import re
 
@@ -112,13 +112,14 @@ def get_ai_service_from_answer(answer):
     if match:
         ai_service = match.group(1).strip()
         print(ai_service)
+        return ai_service
     else:
         print("AI 서비스 정보를 찾을 수 없습니다.")
 
 def create_consultion_result(industry, company_size, pain_point):
     result = "1. 경쟁사 AI 도입 사례\n"
 
-    result += get_industry_example_content(industry = industry, company_size = company_size)
+    result += get_industry_example_content(industry=industry, company_size=company_size)
 
     ai_service = None
     retry = False
@@ -128,20 +129,19 @@ def create_consultion_result(industry, company_size, pain_point):
         ai_service = get_ai_service_from_answer(recommend_ai_answer)
         retry = True # 재 요청 여부를 True로 변환, LLM 요청 프롬프트에서 형식 중요 추가
 
-
-    result += get_ai_service_require_data(industry, ai_service) # 필요 데이터 내용 생성
-
-
-
-
-
-def get_ai_service_require_data(industry, ai_service):
     response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Data, industry=industry)
 
-    rag_data = extract_and_merge_answer(response)
+    data_category_rag = extract_and_merge_answer(response)
+
+    result += get_ai_service_require_data(industry, ai_service, data_category_rag) # 필요 데이터 내용 생성
+
+    result += get_ai_service_process(ai_service=ai_service, data_category_rag=data_category_rag, industry=industry, company_size=company_size)
+
+
+def get_ai_service_require_data(industry, ai_service, rag_data):
 
     prompt = f"""
-다음은 {ai_service}를 구현하기 위해 필요한 데이터 목록입니다. 제공된 RAG 데이터를 이용해서 필요한 데이터 목록들을 전문적이고 명확하게 정리해 주세요. 추가적인 정보나 추측 없이, 주어진 데이터에 기반하여 내용을 구성해야 합니다. PDF 보고서에 포함될 수 있도록 구조화된 형식으로 작성해 주세요. 필요한 데이터의 대주제, 소주제, 예시들을 포함해 주세요.
+다음은 {industry.get_string} 기업에서 {ai_service}를 구현하기 위해 필요한 데이터 목록입니다. 제공된 RAG 데이터를 이용해서 필요한 데이터 목록들을 전문적이고 명확하게 정리해 주세요. 추가적인 정보나 추측 없이, 주어진 데이터에 기반하여 내용을 구성해야 합니다. PDF 보고서에 포함될 수 있도록 구조화된 형식으로 작성해 주세요. 필요한 데이터의 대주제, 소주제, 예시들을 포함해 주세요.
 ---
 **RAG 데이터:**
 {rag_data}
@@ -158,10 +158,44 @@ def get_ai_service_require_data(industry, ai_service):
     answer = get_chat_gpt_answer(prompt)
 
     print(answer)
+    return answer
 
+def get_ai_service_process(ai_service, data_category_rag, industry, company_size):
+    response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Process)
 
+    rag_data = extract_and_merge_answer(response)
+
+    prompt = f"""
+다음은 {company_size.get_string} {industry.get_string}에서 {ai_service}를 구현하기 위한 프로세스 과정과 활용할 데이터 입니다. 제공된 RAG 데이터들을 이용해서 {ai_service} 구현 프로세스를 전문적이고 명확하게 정리해 주세요. 추가적인 정보나 추측 없이, 주어진 데이터에 기반하여 내용을 구성해야 합니다. PDF 보고서에 포함될 수 있도록 구조화된 형식으로 작성해 주세요.
+---
+**RAG 데이터 - 프로세스:**
+{rag_data}
+
+---
+**RAG 데이터 - 활용 데이터:**
+{data_category_rag}
+
+---
+**요청 사항:**
+- 추가적인 정보나 추측 없이 제공된 데이터만을 사용해 주세요.
+- PDF 보고서에 적합하도록 깔끔하고 읽기 쉬운 형식으로 작성해 주세요.
+- 답변은 순차적으로, 어떤 과정이 필요한지 자세하게 답변해 주세요.
+- 주어진 기업 규모와 산업 종류에 맞춰서 프로세스를 작성해 주세요.
+- 답변은 개조식으로 작성해주세요.
+"""
+    print(prompt)
+
+    answer = get_chat_gpt_answer(prompt)
+
+    print(answer)
+    return answer
 
 # merge_industry_example(industry=Industry.Retail, company_size=CompanySize.MEDIUM, category=Category.Industry)
 # get_industry_example_content(Industry.Retail, CompanySize.MEDIUM)
 # get_recommend_ai_service(pain_point="매출 증대를 위한 고객 행동 분석", industry=Industry.Retail, company_size=CompanySize.MEDIUM, retry=True)
-get_ai_service_require_data(Industry.Retail, "딥러닝")
+# get_ai_service_require_data(Industry.Retail, "딥러닝")
+
+# ai_service = "매출 증대를 위한 고객 행동 분석"
+# response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Data, industry=Industry.Retail)
+# data_category_rag = extract_and_merge_answer(response)
+# get_ai_service_process(ai_service, data_category_rag, Industry.Retail, company_size=CompanySize.MEDIUM)
