b2d9313d72c01b50ec299a30a495ca855defc5e4
junhaa <2171326@hansung.ac.kr>
2024-11-21 07:39:40 +0900

    :recycle: REFACTOR. 응답 결과 생성 함수 분리

--- llm/generate_result.py
+++ llm/generate_result.py
@@ -9,35 +9,35 @@ OPENAI_MAX_TOKENS = 4096
 
 PRINT_REQUEST_PROMPT = False # 프롬프트 출력 여부
 
+CONSULTING_RESULT_TITLES = ["1. 경쟁사 AI 도입 사례", "2. Pain Point 관련 추천 AI", "3. AI 도입 시 필요 데이터", "4. AI 도입 프로세스", "5. 예상 비용 및 ROI"]
+
 # 컨설팅 데이터 생성
 def create_consulting_result(industry, company_size, pain_point):
-    result = "1. 경쟁사 AI 도입 사례\n"
-    result += get_industry_example_content(industry=industry, company_size=company_size)
+    result = ""
+    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[0], content=get_industry_example_content(industry=industry, company_size=company_size)) # AI 사례
 
-    ai_service = None
-    retry = False
-
-    recommend_ai_answer = None;
+    recommend_ai_answer = None # 추천 AI 서비스 LLM 응답 결과
+    ai_service = None # 응답 결과에서 추출 된 추천 AI 서비스 (LLM이 응답 형식에 맞게 답변하는 경우 추출)
+    retry = False # 추천 AI 서비스 추출 여부
 
     while ai_service is None: # 만약 ai_service를 추출할 수 없으면 다시 요청
         recommend_ai_answer = get_recommend_ai_service(pain_point= pain_point, industry=industry, company_size=company_size, retry= retry)
         ai_service = get_ai_service_from_answer(recommend_ai_answer)
         retry = True # 재 요청 여부를 True로 변환, LLM 요청 프롬프트에서 형식 중요 추가
 
-    result += "\n\n2. Pain Point 관련 추천 AI\n"
-    result += recommend_ai_answer
+    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[1], content=recommend_ai_answer)
 
-    response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Data, industry=industry)
-    data_category_rag = extract_and_merge_answer(response)
+    data_category_rag_datas = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Data, industry=industry)
+    data_category_rag = extract_and_merge_answer(data_category_rag_datas)
 
-    result += "\n\n3. AI 도입 시 필요 데이터\n"
-    result += get_ai_service_require_data(industry, ai_service, data_category_rag) # 필요 데이터 내용 생성
 
-    result += "\n\n4. AI 도입 프로세스\n"
-    result += get_ai_service_process(ai_service=ai_service, data_category_rag=data_category_rag, industry=industry, company_size=company_size)
+    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[2], content=get_ai_service_require_data(industry, ai_service, data_category_rag))
 
-    result += "\n\n5. 예상 비용 및 ROI\n"
-    result += get_ai_service_ROI(ai_service=ai_service, industry=industry, company_size=company_size, recommend_ai_answer=recommend_ai_answer)
+    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[3],
+                          content=get_ai_service_process(ai_service=ai_service, data_category_rag=data_category_rag, industry=industry, company_size=company_size))
+
+    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[4],
+                          content=get_ai_service_ROI(ai_service=ai_service, industry=industry, company_size=company_size, recommend_ai_answer=recommend_ai_answer))
 
     return result
 
@@ -169,3 +169,8 @@ def get_ai_service_from_answer(answer):
         return ai_service
     else:
         print("AI 서비스 정보를 찾을 수 없습니다.")
+
+
+# 컨설팅 결과 데이터 내용 추가
+def append_result_content(result, title, content):
+    result += f"{title}\n{content}\n\n"
