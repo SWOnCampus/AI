274e0db8058eeaa6f94438a42045a7301bde05e9
junhaa <2171326@hansung.ac.kr>
2024-11-21 07:21:47 +0900

    :recycle: REFACTOR. 코사인 유사도 기반 Hallucination 검증 구조 변경
    - 패키지 위치 변경

--- hallucination/validate_hallucination.py
+++ hallucination/validate_hallucination.py
@@ -1,4 +1,9 @@
 from bert_score import score
+from embeddings.sentence_transform import sentence_embedding
+from scipy.spatial.distance import cosine
+
+
+HALLUCINATION_BOUND = 0.6
 
 def validate_hallucination(rag_data, answer):
 
@@ -18,8 +23,31 @@ def validate_hallucination(rag_data, answer):
 
     # print(f":::: 할루시네이션 검증 유사도 : {similarity * 100}% ::::\n")
 
-    if(F1.mean().item() >= 0.6):
+    if(F1.mean().item() >= HALLUCINATION_BOUND):
         return True
     else:
         return False
 
+
+
+def validate_hallucination_cosine_similarity(rag_data, answer):
+    rag_embedding = sentence_embedding(rag_data)
+    answer_embedding = sentence_embedding(answer)
+    print("::::: 입력 RAG 데이터 :::::\n")
+    print(rag_data + "\n\n")
+
+    print("::::: LLM 반환 내용 :::::\n")
+    print(answer + "\n\n")
+
+    similarity = get_cosine_similarity(rag_embedding, answer_embedding)
+
+    print(f":::: 할루시네이션 검증 유사도 : {similarity * 100}% ::::\n")
+
+    if(similarity < HALLUCINATION_BOUND):
+        return False
+    return True
+
+
+def get_cosine_similarity(vector1, vector2):
+    cosine_similarity = cosine(vector1, vector2)
+    return cosine_similarity
