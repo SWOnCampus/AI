300a2f091d528731b3e303cda25760256ca564c7
junhaa <2171326@hansung.ac.kr>
2024-11-21 08:15:22 +0900

    :recycle: REFACTOR. Hallucination 검증 및 LLM 생성 코드 분리
    - Hallucination 검증 실패 시 LLM 생성 재요청 코드 분리

--- llm/generate_result.py
+++ llm/generate_result.py
@@ -7,135 +7,90 @@ import re
 OPENAI_MODEL = "gpt-3.5-turbo"
 OPENAI_MAX_TOKENS = 4096
 
-PRINT_REQUEST_PROMPT = False # 프롬프트 출력 여부
+PRINT_REQUEST_PROMPT = True # 프롬프트 출력 여부
 
-CONSULTING_RESULT_TITLES = ["1. 경쟁사 AI 도입 사례", "2. Pain Point 관련 추천 AI", "3. AI 도입 시 필요 데이터", "4. AI 도입 프로세스", "5. 예상 비용 및 ROI"]
+CONSULTING_RESULT_TITLES = ["1. 경쟁사 AI 도입 사례", "2. Pain Point 관련 추천 AI", "3. AI 도입 시 필요 데이터", "4. AI 도입 프로세스", "5. 예상 비용 및 ROI"] # 컨설팅 내용 별 타이틀 목록
 
 # 컨설팅 데이터 생성
 def create_consulting_result(industry, company_size, pain_point):
-    result = ""
-    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[0], content=get_industry_example_content(industry=industry, company_size=company_size)) # AI 사례
+    result_contents = []
+    result_contents.append(get_result_content(title=CONSULTING_RESULT_TITLES[0], content=get_industry_example_content(industry=industry, company_size=company_size))) # AI 사례
 
-    recommend_ai_answer, ai_service = get_ai_service_datas(pain_point=pain_point, industry=industry, company_size=company_size) # 추천 AI 서비스
+    recommend_ai_answer, ai_service = get_ai_service_datas(pain_point=pain_point, industry=industry, company_size=company_size)
 
-    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[1], content=recommend_ai_answer)
+    result_contents.append(get_result_content(title=CONSULTING_RESULT_TITLES[1], content=recommend_ai_answer)) # 추천 AI 서비스
 
     data_category_rag = get_category_rag_data(ai_service= ai_service, industry=industry)
 
-    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[2], content=get_ai_service_require_data(industry, ai_service, data_category_rag))
+    result_contents.append(get_result_content(title=CONSULTING_RESULT_TITLES[2], content=get_ai_service_require_data(industry, ai_service, data_category_rag))) # AI 도입 시 필요 데이터
 
-    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[3],
-                          content=get_ai_service_process(ai_service=ai_service, data_category_rag=data_category_rag, industry=industry, company_size=company_size))
+    result_contents.append(get_result_content(title=CONSULTING_RESULT_TITLES[3],
+                       content=get_ai_service_process(ai_service=ai_service, data_category_rag=data_category_rag, industry=industry, company_size=company_size))) # AI 도입 프로세스
 
-    append_result_content(result=result, title=CONSULTING_RESULT_TITLES[4],
-                          content=get_ai_service_ROI(ai_service=ai_service, industry=industry, company_size=company_size, recommend_ai_answer=recommend_ai_answer))
+    result_contents.append(get_result_content(title=CONSULTING_RESULT_TITLES[4],
+                       content=get_ai_service_ROI(ai_service=ai_service, industry=industry, company_size=company_size, recommend_ai_answer=recommend_ai_answer))) # 예상 비용 및 ROI
 
-    return result
+    return "".join(result_contents)
+
 
 
 
 # 경쟁사 AI 도입 사례 내용 생성
 def get_industry_example_content(industry, company_size):
-    while (True):
-         examples = merge_industry_example(industry=industry, company_size=company_size, category=Category.Industry)
+    rag_data = merge_industry_example(industry=industry, company_size=company_size, category=Category.Industry)
 
-         prompt = generate_industry_examples_prompt(industry=industry, company_size=company_size, examples=examples)
-         answer = get_chat_gpt_answer(prompt)
+    prompt = generate_industry_examples_prompt(industry=industry, company_size=company_size, examples=rag_data)
 
-         if validate_hallucination(examples, answer) is True:
-             return answer
+    return get_LLM_answer_with_validate(prompt=prompt, rag_data=rag_data)
 
-# 경쟁사 AI 도입 사례 프롬프트 추가 내용
-def merge_industry_example(industry, company_size, category):
-    response = get_similar_qna_data(data_size=5, company_size= company_size, category= category, industry= industry)
-
-    return extract_and_merge_answer(response)
-
-# RAG 데이터 합쳐서 반환
-def extract_and_merge_answer(response):
-    source = [hit["_source"]["answer"] for hit in response["hits"]["hits"]]
-    examples = ""
-
-    for example in source:
-        examples += example + "\n"
-
-    return examples
 
 
 # pain point 기반 추천 AI 서비스
 def get_recommend_ai_service(pain_point, industry, company_size, retry):
-    while(True):
-        response = get_similar_qna_data(data_size=5, company_size=company_size, category=Category.PainPoints, industry= industry, question=pain_point)
+    response = get_similar_qna_data(data_size=5, company_size=company_size, category=Category.PainPoints,
+                                    industry=industry, question=pain_point)
 
-        examples = extract_and_merge_answer(response)
+    rag_data = extract_and_merge_answer(response)
 
-        prompt = generate_ai_service_prompt(pain_point=pain_point, industry=industry, company_size=company_size, examples= examples, retry= retry)
-
-        answer = get_chat_gpt_answer(prompt=prompt, temperature=0.3)
-
-        if validate_hallucination(examples, answer) is True:
-             return answer
-
-        return answer;
+    prompt = generate_ai_service_prompt(pain_point=pain_point, industry=industry, company_size=company_size,
+                                        examples=rag_data, retry=retry)
+    return get_LLM_answer_with_validate(prompt=prompt, rag_data=rag_data)
 
 
 # AI 서비스 관련 필요 데이터
 def get_ai_service_require_data(industry, ai_service, rag_data):
-    while(True):
-        prompt = generate_ai_service_require_data_prompt(industry=industry, ai_service=ai_service, rag_data=rag_data)
+    prompt = generate_ai_service_require_data_prompt(industry=industry, ai_service=ai_service, rag_data=rag_data)
 
-        answer = get_chat_gpt_answer(prompt)
-
-        if validate_hallucination(rag_data, answer) is True:
-             return answer
-
-        return answer
+    return get_LLM_answer_with_validate(prompt=prompt, rag_data=rag_data)
 
 
 # AI 서비스 구축 프로세스
 def get_ai_service_process(ai_service, data_category_rag, industry, company_size):
-    while(True):
-        response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Process)
+    response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.Process)
 
-        rag_data = extract_and_merge_answer(response)
+    rag_data = extract_and_merge_answer(response)
 
-        prompt = generate_ai_service_process_prompt(company_size=company_size, industry=industry, ai_service=ai_service, rag_data= rag_data, data_category_rag_data=data_category_rag)
+    prompt = generate_ai_service_process_prompt(company_size=company_size, industry=industry, ai_service=ai_service,
+                                                rag_data=rag_data, data_category_rag_data=data_category_rag)
 
-        answer = get_chat_gpt_answer(prompt)
-
-        if validate_hallucination(rag_data + data_category_rag, answer) is True:
-             return answer
-
-        return answer
+    return get_LLM_answer_with_validate(prompt, rag_data + data_category_rag)
 
 # AI 서비스 관련 예상 ROI
 def get_ai_service_ROI(ai_service, industry, company_size, recommend_ai_answer):
-    while(True):
-        response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.CostROI, company_size=company_size, industry=industry)
+    response = get_similar_qna_data(data_size=1, question=ai_service, category=Category.CostROI,
+                                    company_size=company_size, industry=industry)
 
-        rag_data = extract_and_merge_answer(response)
+    rag_data = extract_and_merge_answer(response)
+    prompt = generate_ai_service_ROI_prompt(company_size=company_size, industry=industry, ai_service=ai_service,
+                                            recommend_ai_answer=recommend_ai_answer, rag_data=rag_data)
 
-        prompt = generate_ai_service_ROI_prompt(company_size=company_size, industry=industry, ai_service=ai_service, recommend_ai_answer=recommend_ai_answer, rag_data=rag_data)
-
-        answer = get_chat_gpt_answer(prompt)
-
-        if validate_hallucination(ai_service + rag_data, answer) is True:
-             return answer
-
-        return answer
+    return get_LLM_answer_with_validate(prompt, rag_data + ai_service)
 
 
 # AI 컨설팅 데이터 요약
 def get_summary_result(result):
-    while(True):
-        prompt = generate_summary_result_prompt(result)
-        answer = get_chat_gpt_answer(prompt)
-
-        if validate_hallucination(result, answer) is True:
-             return answer
-
-        return answer
-
+    prompt = generate_summary_result_prompt(result)
+    return get_LLM_answer_with_validate(prompt=prompt, rag_data=result)
 
 # CHAT_GPT_응답 생성
 def get_chat_gpt_answer(prompt, temperature=0.7):
@@ -151,6 +106,25 @@ def get_chat_gpt_answer(prompt, temperature=0.7):
     )
 
     return response['choices'][0]['message']['content']
+
+
+# RAG 데이터 합쳐서 반환
+def extract_and_merge_answer(response):
+    source = [hit["_source"]["answer"] for hit in response["hits"]["hits"]]
+    examples = ""
+
+    for example in source:
+        examples += example + "\n"
+
+    return examples
+
+# 경쟁사 AI 도입 사례 프롬프트 추가 내용
+def merge_industry_example(industry, company_size, category):
+    response = get_similar_qna_data(data_size=5, company_size= company_size, category= category, industry= industry)
+
+    return extract_and_merge_answer(response)
+
+
 
 # "AI 서비스" 항목 추출
 def get_ai_service_from_answer(answer):
@@ -184,6 +158,15 @@ def get_category_rag_data(ai_service, industry):
 
     return data_category_rag
 
+# 할루시네이션 검증을 포함하여 LLM 답변
+def get_LLM_answer_with_validate(prompt, rag_data, temperature=0.7):
+    while(True):
+        answer = get_chat_gpt_answer(prompt=prompt, temperature=temperature)
+
+        if(validate_hallucination(rag_data=rag_data, answer=answer) is True):
+            return answer
+
+
 # 컨설팅 결과 데이터 내용 추가
-def append_result_content(result, title, content):
-    result += f"{title}\n{content}\n\n"
+def get_result_content(title, content):
+    return f"{title}\n{content}\n\n"
