1e5f1dc867976febefd4e5a9a3f1e5bb5b7a875f
junhaa <2171326@hansung.ac.kr>
2024-11-23 16:01:38 +0900

    :sparkles: FEAT. RAG 데이터 및 LLM 생성 데이터 소켓 전송

---
+++ log/send_log_data.py
@@ -0,0 +1,24 @@
+import json
+from globals import connected_clients
+
+async def send_info(id, title, data_type, content):
+    # 전송할 데이터 예시
+    data = {
+        'id': id,
+        'title': title,
+        'data_type': data_type,
+        'content': content
+    }
+
+
+    for client in connected_clients:
+        try:
+            await client.send_text(json.dumps(data, ensure_ascii=False))
+        except Exception as e:
+            connected_clients.remove(client)
+
+
+async def send_info_test():
+    while True:
+        for client in connected_clients:
+            await client.send_text(f"data")
