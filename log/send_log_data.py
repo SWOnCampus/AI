432adefbf68bdc437109a508ba6baf41d62412c4
junhaa <2171326@hansung.ac.kr>
2024-11-28 00:36:54 +0900

    :bug: FIX. 로그 전달 시 처음 데이터 전달 안되는 오류 해결
    - client 예외 catch 부분에서 remove 시 list가 순차적으로 돌지 않는 경우 발생

--- log/send_log_data.py
+++ log/send_log_data.py
@@ -1,5 +1,9 @@
 import json
 from globals import connected_clients
+import asyncio
+from fastapi import WebSocketDisconnect
+
+lock = asyncio.Lock()
 
 async def send_info(id, title, data_type, content):
     # 전송할 데이터 예시
@@ -10,14 +14,19 @@ async def send_info(id, title, data_type, content):
         'content': content
     }
 
+    remove_list = [];
 
     for client in connected_clients:
         try:
             await client.send_text(json.dumps(data, ensure_ascii=False))
-        except Exception as e:
-            connected_clients.remove(client)
-            continue;
+        except WebSocketDisconnect as e:
+            remove_list.append(client)
+            continue
 
+
+    for remove in remove_list:
+        async with lock:
+            connected_clients.remove(remove)
 
 async def send_info_test():
     while True:
